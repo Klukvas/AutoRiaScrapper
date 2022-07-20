96c479d232c761c5957c8c9b7871f1e9baf0df73
Klukvas <pavlenkoandrey56@gmail.com>
2022-07-21 01:42:14 +0300

    refactor infrastructure

--- Scrapper/exceptions.py
+++
@@ -1,8 +0,0 @@
-class AutoRiaException(Exception):
-    # Class for exceptions of processing cars data from AutoRia website
-    def __init__(self, message):
-        self.message = message
-        super().__init__(message)
-
-    def __str__(self):
-        return f"Class called for error: {self.message}"
---
+++ manage.py
@@ -0,0 +1,33 @@
+print('asd1')
+import sys
+
+from CarChooser.Scrapper.AutoRia import ria_parser
+from CarChooser.Scrapper.query import Query
+from CarChooser.Scrapper.serializer import Serializer
+from CarChooser.Configs.logger import Logger
+from CarChooser.Configs.config_reader import get_config
+
+from CarChooser.RestApi import api_runner
+
+
+def start():
+    argument_list = sys.argv[1:]
+    log = Logger().custom_logger()
+    if argument_list:
+        query = Query(log)
+        serializer = Serializer(log)
+        for argument in argument_list:
+            if argument == '-AutoRia':
+                print('asd')
+                config = get_config('AutoRia')
+                ria_parser.run(log, config, query, serializer)
+            elif argument == '-runserver':
+                api_runner.runserver()
+            else:
+                log.error(f"Can not find your argument: {argument}")
+    else:
+        log.error(f'With run command u must to pass the one of arguments: ["-AutoRia"]')
+
+
+if __name__ == '__main__':
+    start()
